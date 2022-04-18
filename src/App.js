db1dff0ebb7bc505f86d7ce1866fad43f321b333
Elena Tursunova <elenatursunova@gmail.com>
2022-04-18 15:53:03 +0300

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import Listing from './components/Listing';
 import './App.css';
+import etsy from './data/etsy';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <Listing items={etsy} />
+      </div>
   );
 }
 
